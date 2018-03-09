3321e18b691086cb1999609978260273e5ce5391
Davide Marcato <davide.marcato@lnl.infn.it>
2018-03-09 16:57:16 +0100

    Tweaks to setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(name='pysmlib',
       version='2.0.0',
       description='A library to create finite state machines running as daemons and sharing resources',
-      #url='http://github.com/storborg/funniest',
+      url='https://baltig.infn.it/epicscs/pysmlib',
       author='Damiano Bortolato - Davide Marcato',
       author_email='davide.marcato@lnl.infn.it',
       license='GPLv3',
       packages=['smlib'],
+      install_requires=['pyepics'],
       zip_safe=False)
