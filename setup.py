b55a4f29c119dee3414636ee0518b5ee065c4a3b
Davide Marcato <davide.marcato@lnl.infn.it>
2018-08-28 17:16:49 +0200

    pypi description and url updated

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 import versioneer
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(name='pysmlib',
       version=versioneer.get_version(),
       description='A library to create event driven finite state machines for EPICS, running as daemons and sharing resources',
-      url='https://baltig.infn.it/epicscs/pysmlib',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/darcato/pysmlib',
       author='Damiano Bortolato - Davide Marcato',
       author_email='davide.marcato@lnl.infn.it',
       license='GPLv3',
