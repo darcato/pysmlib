180387abbe973000145124bca0735f6e744fdb5f
Davide Marcato <davide.marcato@lnl.infn.it>
2020-09-14 10:38:38 +0200

    Updating automatic testing with coverage

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,13 @@
 import nox
 
+# HOW TO RUN TEST SUITE:
+# - run `pytest` inside the tests directory to test in the current python env
+# - OR run `nox` from the top directory to use multiple python envs
+# - OR `gitlab-runner exec docker nox` to test the gitlab CI/CD
+
 @nox.session(python=["3.6", "3.7", "3.8"], venv_backend="conda")
 def auto_tests(session):
-    session.conda_install("--channel", "conda-forge", "pcaspy", "pytest", "pyepics")
+    session.conda_install("--channel", "conda-forge", "pcaspy", "pytest", "pytest-cov", "pyepics")
     session.install("-e", ".", "--no-deps")
-    session.run("pytest")
+    session.cd("tests")
+    session.run("pytest", "--cov=smlib")
